9d2c64eb88d7d0ebe91e3e59777e7117416d20ee
Mykola Akimov <mykola.akimov@bs2.lt>
2025-01-08 16:33:03 +0400

    removed guide

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,13 +9,8 @@ const App = () => {
 
   return (
     <>
-      <div className='fixed bottom-0 left-0 top-0'>
-        <Devbar />
-      </div>
-      <div className='ml-[700px]'>
-        { token && <Navbar />}
-        <Outlet />
-      </div>
+      {token && <Navbar />}
+      <Outlet />
     </>
   );
 };
